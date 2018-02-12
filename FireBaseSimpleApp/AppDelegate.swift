68c0f4795120f4d1927edda0bc08091dd59f3893
Pepe <krakenn@krakenns-imac.local>
2018-02-12 12:32:58 -0600

    Initial Commit

---
+++ FireBaseSimpleApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FireBaseSimpleApp
+//
+//  Created by Krakenn  on 2/12/18.
+//  Copyright © 2018 Krakenn . All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
